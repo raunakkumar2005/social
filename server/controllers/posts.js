7b4bc6815a9bfab471302dd0b8966e0877a1e7aa
raunak <raunakmar2005@gmail.com>
2024-01-02 16:32:19 +0530

    commit

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -45,6 +45,16 @@ export const getUserPosts = async (req, res) => {
     res.status(404).json({ message: err.message });
   }
 };
+// get single post
+export const getpostbyid = async (req,res)=>{
+  try {
+    const { postId } = req.params;
+    const post = await Post.find({ postId });
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+}
 
 /* UPDATE */
 export const likePost = async (req, res) => {
